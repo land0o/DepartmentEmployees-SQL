8c168c11e1e76c13573313c422906294e21a369e
curtis <crutchc@yahoo.com>
2019-10-28 17:14:17 -0500

    Repository todo's completed

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿--INSERT INTO Employee (FirstName, LastName, DepartmentId) VALUES ('Jane', 'Smite', 2);
+--delete from Employee where Id = 4;
+--SELECT FirstName FROM Employee WHERE Id = Id;
+--SELECT * FROM Employee;
+--SELECT FirstName, LastName FROM Employee;
+--SELECT Id, FirstName, LastName, DepartmentId FROM Employee;
+--SELECT Id, FirstName, LastName, DepartmentId FROM Employee; 
+--INNER JOIN Department ON DeptName;
+--SELECT e.Id, e.FirstName, e.LastName, e.DepartmentId, d.DeptName FROM Employee e INNER JOIN Department d ON e.DepartmentID = d.id;
+--SELECT e.Id, e.FirstName, e.LastName, e.DepartmentId, d.DeptName FROM Employee e INNER JOIN Department d ON e.DepartmentID = d.id;
